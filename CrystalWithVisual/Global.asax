86cf0ce85c1177d90d217dd59693b4c67760fd80
itware1030 <itware1030@ITWI018>
2018-02-12 17:29:42 +0530

    Add project files.

---
+++ CrystalWithVisual/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrystalWithVisual.MvcApplication" Language="C#" %>
---
+++ CrystalWithVisual/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrystalWithVisual.Startup))]
+namespace CrystalWithVisual
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
